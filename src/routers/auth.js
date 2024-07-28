992fa645fafcd5542af59262d48c99311de75107
haibvph33557 <haibvph33557@fpt.edu.vn>
2024-07-29 00:47:23 +0700

    backup

--- src/routers/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { signin, singup } from '../controller/auth';
-
-const router = express.Router();
-router.post(`/singup`, singup);
-router.post(`/singin`, signin);
-
-export default router;
